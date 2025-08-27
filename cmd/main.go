3dd01636414f48d03dfc55457f0e96738f0ccbfe
Almaz Fedorov <fedoro3valmaz@gmail.com>
2025-08-28 01:34:26 +0300

    3.5. Сервис конфигурации

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"go/go-fiber/config"
 	"go/go-fiber/internal/home"
+	"log"
 
 	"github.com/gofiber/fiber/v3"
 )
 
 func main() {
+	config.Init()
+	dbConf := config.NewDatabaseConfig()
+	log.Println(dbConf)
+
 	app := fiber.New()
 
 	home.NewHandler(app)
