90388a2f9fd928a3a4de9c4f028e4c8168cb72d5
Almaz Fedorov <fedoro3valmaz@gmail.com>
2025-09-05 22:33:41 +0300

    5.1. Простой шаблон

--- internal/home/handler.go
+++ internal/home/handler.go
@@ -1,6 +1,9 @@
 package home
 
 import (
+	"bytes"
+	"text/template"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/rs/zerolog"
 )
@@ -21,7 +24,16 @@ func NewHandler(router fiber.Router, customLogger *zerolog.Logger) {
 }
 
 func (h *HomeHandler) home(c *fiber.Ctx) error {
-	return fiber.NewError(fiber.StatusBadRequest, "Limit params is undefined")
+	tmpl, err := template.New("test").Parse("{{.Count}} - число пользователей")
+	data := struct{ Count int }{Count: 1}
+	if err != nil {
+		return fiber.NewError(fiber.StatusBadRequest, "Template error")
+	}
+	var tpl bytes.Buffer
+	if err := tmpl.Execute(&tpl, data); err != nil {
+		return fiber.NewError(fiber.StatusBadRequest, "Template compile error")
+	}
+	return c.Send(tpl.Bytes())
 }
 
 func (h *HomeHandler) error(c *fiber.Ctx) error {
